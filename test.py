7518b4907f1322cf61246cea9849865633bed522
Seyed Danial Movahed <movaheddanial@gmail.com>
2023-08-03 00:48:43 +0330

    Change to chrome

--- test.py
+++ test.py
@@ -5,14 +5,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep
 
-proxyhost = "127.0.0.1"
-proxyport = 40000
-options = webdriver.FirefoxOptions()
+options = webdriver.ChromeOptions()
 options.add_argument('--headless')
-options.set_preference('network.proxy.type', 1)
-options.set_preference('network.proxy.socks', proxyhost)
-options.set_preference('network.proxy.socks_port', proxyport)
-driver = webdriver.Firefox(options=options)
+options.add_argument('--proxy-server=socks5://127.0.0.1:40000')
+driver = webdriver.Chrome(options=options)
 driver.get("https://ping.eu")
 print("piiing!")
 driver.save_screenshot('sample_screenshot_1.png')
